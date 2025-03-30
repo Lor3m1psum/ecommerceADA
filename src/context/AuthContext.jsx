07d5c5d3d622cae420edb3f8df1c93f2709ab837
Lor3m1psum <lorena.rosales.pajaro87@gmail.com>
2025-03-29 21:27:21 -0500

    configuration

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,25 +1,11 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { createContext, useContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
 import { auth } from "../firebase/config";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  //   const [error, setError] = useState(null);
-
-  //   const login = ({ email, password }) => {
-  //     if (username === user_.username && password === user_.password) {
-  //       setUser({ username, password });
-  //       setError(null);
-  //     } else {
-  //       setError("Invalid username or password");
-  //     }
-  //   };
-
-  //   const logout = () => {
-  //     setUser(null);
-  //   };
 
   const registerUser = async ({ email, password }) => {
     try {
@@ -29,6 +15,7 @@ export const AuthProvider = ({ children }) => {
         password
       );
       const user = userCredential.user;
+      setUser(user);
       return user;
     } catch (error) {
       const errorCode = error.code;
