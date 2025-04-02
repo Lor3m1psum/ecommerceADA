b3ae199cc7c8eac17a2297980e082a25f72f6184
Lor3m1psum <lorena.rosales.pajaro87@gmail.com>
2025-04-02 09:12:13 -0500

    review validations

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,29 +1,29 @@
 export const password = {
   required: true,
   pattern: {
-    value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/,
+    value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/,
     message:
       "Password must have at least one uppercase letter, one lowercase letter, and one number",
   },
   minLength: {
-    value: 4,
-    message: "Password must have at least 4 characters",
+    value: 8,
+    message: "Password must have at least 8 characters",
   },
   maxLength: {
-    value: 6,
-    message: "Password must have at most 6 characters",
+    value: 20,
+    message: "Password must have at most 20 characters",
   },
 };
 
 export const username = {
   required: true,
   minLength: {
-    value: 4,
-    message: "Username must have at least 4 characters",
+    value: 8,
+    message: "Username must have at least 8 characters",
   },
   maxLength: {
-    value: 20,
-    message: "Username must have at most 20 characters",
+    value: 16,
+    message: "Username must have at most 16 characters",
   },
 };
 
